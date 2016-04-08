b565e2ae14b02d4ba7ebd579935d55546d28e58e
RIPDieter <crapgurke@gmail.com>
2016-04-08 23:09:38 +0200

    Prototype
    draw road, spawn car, car moving

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Projekt4GruppeA.Classes;
 
 namespace Projekt4GruppeA
 {
@@ -20,10 +23,41 @@ namespace Projekt4GruppeA
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        Ellipse body1 = new Ellipse();
+        int xPosition = 10;
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        // test 2
+
+        public void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            CarCasual c1 = new CarCasual();
+
+            body1.Width = 10;
+            body1.Height = 10;
+            body1.Fill = Brushes.Black;
+
+            
+
+            RoadCasual.Children.Add(body1);
+        }
+
+        private void btnTimer_Click(object sender, RoutedEventArgs e)
+        {
+
+            timer.Interval = TimeSpan.FromMilliseconds(40);
+            timer.Tick += timerTick;
+            timer.Start();
+        }
+
+        public void timerTick(object sender, EventArgs e)
+        {
+            Canvas.SetLeft(body1, xPosition);
+            xPosition += 10;
+        }
     }
 }
