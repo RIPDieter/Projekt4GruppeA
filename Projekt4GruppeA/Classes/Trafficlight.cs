818307f33e51623d40727e11dd79e25ff83b703b
RIPDieter <crapgurke@gmail.com>
2016-05-11 22:27:22 +0200

    Fixed Ampel
    Traffic starts & stops

---
+++ Projekt4GruppeA/Classes/Trafficlight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Projekt4GruppeA.Classes
+{
+    public class Trafficlight
+    {
+        //Ort
+        public int location { get; set; }
+
+        //Form
+        public Ellipse body { get; set; }
+
+        //Zustand
+        public bool isRed { get; set; }
+        
+        //Konstruktor - im Moment nicht benutzt  
+        public Trafficlight()
+        {
+            Ellipse body = new Ellipse();
+            body.Width = 25;
+            body.Height = 25;
+            isRed = false;
+        }
+    }
+}
--- Projekt4GruppeA/MainWindow.xaml.cs
+++ Projekt4GruppeA/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using Projekt4GruppeA.Classes;
 
 
 namespace Projekt4GruppeA
@@ -34,25 +35,58 @@ namespace Projekt4GruppeA
         DispatcherTimer timer = new DispatcherTimer();
         //Global ID Counter
         public static int idCounter = 0;
-        
+
+        //
+        public int timerCount = 0;
 
         private List<CarCasual> carList = new List<CarCasual>();
 
         public MainWindow()
         {
             InitializeComponent();
+            spawntrafficlight();
         }
+
+        #region BUTTON CLICK EVENTS
 
         public void btnSpawn_Click(object sender, RoutedEventArgs e)
         {
-            spawnCars(1);   
-            spawntrafficlight(1);
+            //spawnCars(1);   
+            //spawntrafficlight(1);
         }
+
+        private void btnTimer_Click(object sender, RoutedEventArgs e)
+        {
+            startTimer(1);
+        }
+
+        #endregion BUTTON CLICK EVENTS
+
+        #region TIMER RELATED
+
+        private void startTimer(int timerSpeed)
+        {
+            timer.Interval = TimeSpan.FromSeconds(timerSpeed);
+            timer.Tick += timerTick;
+            timer.Start();
+        }
+
+        public void timerTick(object sender, EventArgs e)
+        {
+            timerCount++;
+            //spawnCars(Convert.ToInt16(sldSpawn.Value));
+            switchLight();
+            spawnCars(1);
+            moveCars();
+
+        }
+
+
+        #endregion TIMER RELATED
 
         public void spawnCars(int carsToSpawn)
         {
-
-            carsToSpawn = Convert.ToInt16(sldSpawn.Value);
+            //carsToSpawn = Convert.ToInt16(sldSpawn.Value);
 
             Brush[] carColors = new Brush[]
                 {
@@ -64,8 +98,6 @@ namespace Projekt4GruppeA
                 Brushes.Fuchsia
                 };
 
-            for (int j = 0; j < carsToSpawn; j++)
-            {
                 CarCasual car = new CarCasual
                 {
                     speed = 10,
@@ -76,70 +108,32 @@ namespace Projekt4GruppeA
                 car.body.Fill = carColors[rnd.Next(carColors.Length)];
                 carList.Add(car);
 
-                Grid.SetColumn(car.body,0);
-                Grid.SetRow(car.body, 3);
+                Grid.SetColumn(car.body,1);
+                Grid.SetRow(car.body, 5);
                 gr_mainGrid.Children.Add(car.body);
-
-            }
         }
-        public void spawntrafficlight(int trafficlights)
-        {
-            startTimer(1);
-            Brush[] trafficlight = new Brush[]
-            {
-                Brushes.Black,
-                Brushes.DarkRed,
-                Brushes.LightYellow,
-                Brushes.DarkGreen,
-                Brushes.Red,
-            };
-
-
-            Ellipse Kreis = new Ellipse();
-            Kreis.Fill = trafficlight[4];
-            Kreis.Width = 25;
-            Kreis.Height = 25;
-
-            Light.Children.Add(Kreis);
-            if (timer.Equals(20))
-            {
-                Kreis.Fill = trafficlight[3];
-            }
-        }
-
-        private void btnTimer_Click(object sender, RoutedEventArgs e)
-        {
-            startTimer(1);
-        }
-
-        private void startTimer(int timerSpeed)
-        {
-            timer.Interval = TimeSpan.FromSeconds(timerSpeed);
-            timer.Tick += timerTick;
-            timer.Start();
-        }
-
-        public void timerTick(object sender, EventArgs e)
-        {
-            spawnCars(Convert.ToInt16(sldSpawn.Value));
-            moveCars();
-        }
-
+        
         private void moveCars()
         {
             foreach (CarCasual thisCar in carList)
             {
-                var x = Grid.GetColumn(thisCar.body);
-                x++;
-                Grid.SetColumn(thisCar.body,x);
+                var placeOfCar = Grid.GetColumn(thisCar.body);
+                var placeOfTrafficLight = Grid.GetColumn(ampel.body);
+                if (placeOfCar != placeOfTrafficLight || ampel.isRed == false)
+                {
+                    var moveSize = Grid.GetColumn(thisCar.body);
+                    moveSize++;
+                    Grid.SetColumn(thisCar.body, moveSize);
+                }              
             }
         }
 
 
-
+        #region SLIDER
 
         private void sldTime_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            //TODO Umdrehen
             timer.Interval = TimeSpan.FromSeconds(sldTime.Value);
         }
 
@@ -147,6 +141,54 @@ namespace Projekt4GruppeA
         {
 
         }
+        #endregion SLIDER
+
+        #region AMPEL
+
+        Trafficlight ampel = new Trafficlight();
+        public void spawntrafficlight()
+        {
+            ampel.body = new Ellipse();
+
+
+            Brush[] trafficLightColors = new Brush[]
+            {
+                Brushes.Black,         
+                Brushes.Green,
+                Brushes.Red,
+            };
+
+            ampel.body.Fill = trafficLightColors[1];
+
+            Grid.SetColumn(ampel.body, 6);
+            Grid.SetRow(ampel.body, 4);
+            gr_mainGrid.Children.Add(ampel.body);
+        }
+
+        private void switchLight()
+        {
+            if (timerCount % 5 == 0)
+            {
+                //TODO auto change color of ampel when isRed changes
+                if (ampel.isRed == true)
+                {
+                    ampel.isRed = false;
+                    ampel.body.Fill = (new SolidColorBrush(Colors.Green));
+                }
+                else
+                {
+                    ampel.isRed = true;
+                    ampel.body.Fill = (new SolidColorBrush(Colors.Red));
+                }
+
+            }
+        }
+
+
+
+        #endregion AMPEL
+
+
     }
 }
 
