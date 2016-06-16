999a739e73cf1f448e2ce5cf8f4d2cb0be62ccec
sanders21 <sandsch95@gmail.com>
2016-06-16 21:05:15 +0200

    Chart
    man sieht zwar nix, ich kann jetzt nur einmal sagen es funktioniert
    weder mit "image" noch mit "canvas". grundsätzlich bleibt nur über das
    irgendwie über ein grid zu implementieren. ich werd morgn
    weiterprobiern.

--- Projekt4GruppeA/Analysis.xaml.cs
+++ Projekt4GruppeA/Analysis.xaml.cs
@@ -8,7 +8,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Forms;
@@ -23,9 +22,14 @@ namespace Projekt4GruppeA
     public partial class Analysis : Window
     {
 
-        Bitmap bmp;
-        Graphics z;
-        
+        //Bitmap bmp;
+        //Graphics z;
+
+        //const float minX = -0.5f;
+        //const float maxX = 7.5f;
+        //const float minY = -1.1f;
+        //const float maxY = 1.1f;
+
 
         public Analysis()
         {
@@ -33,27 +37,64 @@ namespace Projekt4GruppeA
             
         }
 
-        private void image_Paint(object sender, EventArgs e)
-        {
-            float minX = -1.1f;
-            float maxX = 1.1f;
 
-            float minY = -1.1f;
-            float maxY = 1.1f;
 
-            float breite = maxX - minX;
-            float hoehe = maxY - minY; // Deklarieren von Maßen
 
-            //System.Drawing.Drawing2D.Matrix myMatrix = new System.Drawing.Drawing2D.Matrix();
-            //myMatrix.Scale(image.RenderSize.Width / (double)breite, image.RenderSize.Height / (double)hoehe); // Skalierung der Matrix
-            //myMatrix.Translate(-minX, -minY, MatrixOrder.Prepend);
 
-            System.Drawing.Pen p = new System.Drawing.Pen(System.Drawing.Color.Black, 1f);
+        //private void ImageSource(object sender, PaintEventArgs e)
+        //{
+        //    float breite = maxX - minX;
+        //    float hoehe = maxY - minY;
 
-            z.DrawEllipse(p, 5, 5, 2, 2);
-         
-               
-        }
+        //    z.SmoothingMode = SmoothingMode.AntiAlias;
+
+        //    Matrix chart = new Matrix();
+        //    chart.Scale((float)image.ActualHeight / hoehe, (float)image.ActualWidth / breite);
+
+        //    chart.Translate(-minX, -minY, MatrixOrder.Prepend);
+
+        //    z.Transform = chart;
+
+        //    Pen pkoord = new Pen(Color.Black, 0.01f);
+
+        //    z.DrawLine(pkoord, 0, 0, 7, 0);
+        //    z.DrawLine(pkoord, 0, -1, 0, -1);
+
+        //    PointF[] kurve = new PointF[1000];
+        //    for (int i = 0; i < kurve.Length; i++)
+        //    {
+        //        kurve[i].X = (float)i * 7 / 1000f;
+        //        kurve[i].Y = -(float)Math.Sin(kurve[i].X * 2 / 10f);
+        //    }
+
+        //    Pen pKurve = new Pen(Color.Red, 0.01f);
+        //    z.DrawLines(pKurve, kurve);
+
+
+        //}
+
+
+
+
+
+        //{
+        //    float minX = -1.1f;
+        //    float maxX = 1.1f;
+
+        //    float minY = -1.1f;
+        //    float maxY = 1.1f;
+
+        //    float breite = maxX - minX;
+        //    float hoehe = maxY - minY; // Deklarieren von Maßen
+
+        //System.Drawing.Drawing2D.Matrix myMatrix = new System.Drawing.Drawing2D.Matrix();
+        //myMatrix.Scale(image.RenderSize.Width / (double)breite, image.RenderSize.Height / (double)hoehe); // Skalierung der Matrix
+        //myMatrix.Translate(-minX, -minY, MatrixOrder.Prepend);
+
+
+
+
+    }
 
 
 
@@ -105,41 +146,16 @@ namespace Projekt4GruppeA
 
         #region XY_Axis
 
-        //private void Paint(object sender, PaintEventArgs e)
-        //{
-
-        //    Graphics z = e.Graphics;
-
-        //    System.Drawing.Pen Koord = new System.Drawing.Pen(System.Drawing.Color.Black, 10f);
-
-        //    PointF[] kurve = new PointF[1000];
-        //    for (int i = 0; i < kurve.Length; i++)
-        //    {
-        //        kurve[i].X = (float)i * 7 / 1000f;
-        //        kurve[i].Y = -(float)Math.Sin(kurve[i].X * 5 / 10f);
-        //    }
-
-        //    System.Drawing.Pen pKurve = new System.Drawing.Pen(System.Drawing.Color.Red, 0.01f);
-        //    z.DrawLines(pKurve, kurve);
-
-            
-
-        //}
-
-
-
-
 
 
 
         #endregion XY_Axis
 
 
-
-        }
-
-
     }
+
+
+    
 
 
     
